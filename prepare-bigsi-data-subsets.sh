0d09ad2d346ae8128cf9b5d8e1cde0b040c36b7f
Timo Bingmann <tbgit@panthema.net>
2019-04-18 08:50:59 +0200

    select new bigsi subsets, as sub-subsets

--- prepare-bigsi-data-subsets.sh
+++ prepare-bigsi-data-subsets.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 ################################################################################
 # Script to prepare subsets of the BIGSI experiment set
 #
@@ -13,6 +13,21 @@ if [ "$(basename $PWD)" != "bigsi-data" -o ! -e "cortex" ]; then
     echo "Run in bigsi-data directory"
     exit
 fi
+
+make_list() {
+    if [ ! -e $SCRIPT_DIR/list-bigsi-data-$1.txt ]; then
+        sort -R $SCRIPT_DIR/list-bigsi-$2.txt \
+            | tail -n $1 \
+            > $SCRIPT_DIR/list-bigsi-data-$1.txt
+    fi
+}
+
+make_list 5000 data
+make_list 2500 data-5000
+make_list 1000 data-2500
+make_list 500 data-1000
+make_list 250 data-500
+make_list 100 data-250
 
 for s in 100 250 500 1000 2500 5000; do
     mkdir -p ../bigsi-data$s/cortex/
